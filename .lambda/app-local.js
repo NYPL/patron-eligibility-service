1485a8ff20c1ce896f4e2b3705cda30067a1b6fa
Daniel Appel <dan.s.appel@gmail.com>
2018-07-26 11:29:26 -0400

    Remove ignored files

--- .lambda/app-local.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-const port = 3003
-
-// Require certain argv params to establish what environment to serve:
-require('./lib/local-env-helper')
-
-app.listen(port)
-console.log(`listening on http://localhost:${port}`)
--- .lambda/test/testHelper.test.js
+++
@@ -1,2 +0,0 @@
-require('../app')
-global.expect = require('chai').expect
